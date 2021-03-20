98d3e266937e217f29e6995c6a7d6f3b1db39fdc
Ahmed Zakaria <70409695+AhmedZkaria22@users.noreply.github.com>
2021-03-21 01:24:19 +0200

    Update Plugin.js

--- Js/Plugin.js
+++ Js/Plugin.js
@@ -52,7 +52,3 @@ window.addEventListener("resize",function(){
     document.querySelectorAll(".worksImgs .img-fluid")[1].style.width = 
     document.querySelectorAll(".worksImgs .img-fluid")[0].clientWidth.toString()+"px";
 });
-
-document.querySelector(".toTop").addEventListener("click",function(){
-    window.scrollTo(0,0);
-});
